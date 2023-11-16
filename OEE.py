f32640b3bdf950165d6f659e4513418224766c3a
Shreya Rai <130877780+Shreyarai-1503@users.noreply.github.com>
2023-11-16 16:17:36 +0530

    Updated OEE.py

--- OEE.py
+++ OEE.py
@@ -13,29 +13,19 @@ x=input('Enter your choice:')
 print('----'*20)
 print(Fore.MAGENTA)
 if x=='Yes':
- print('That’s great, as you are already aware that by measuring OEE and the 
-underlying losses, you will gain important insights on how to systematically 
-improve your manufacturing process. OEE is the single best metric for 
-identifying losses, benchmarking progress, and improving the productivity of 
-manufacturing equipment.')
+ print('That’s great, as you are already aware that by measuring OEE and the underlying losses, you will gain important insights on how to systematically improve your manufacturing process. OEE is the single best metric for identifying losses, benchmarking progress, and improving the productivity of manufacturing equipment.')
 elif x=='No':
  print('It’s okay let me explain it to you')
- print('So OEE is a framework of measuring the efficiency and effectiveness of a 
-process, by breaking it down into three constituent components (Availability, 
-11
-Productivity, Quality). OEE helps you identify and measure problems so you can 
-fix it, and it also provides a standardized method of benchmarking process.')
+ print('So OEE is a framework of measuring the efficiency and effectiveness of a process, by breaking it down into three constituent components (Availability, Productivity, Quality). OEE helps you identify and measure problems so you can fix it, and it also provides a standardized method of benchmarking process.')
 print(Fore.RED) 
 print('Now lets look at factors on which OEE depends')
 print('The major 3 factors are\n 1. Availability\n 2. Performance\n 3. Quality')
 print(Fore.BLUE)
-print('Now let's have a look at their formulas')
+print("Now let's have a look at their formulas")
 print()
-print(' Operating Time\n Availability = -------------------\n 
-Planned Production Time ')
+print(' Operating Time\n Availability = -------------------\n Planned Production Time ')
 print(' \n ')
-print(' Ideal Cycle Time × Total Count\n Performance = -------------------
-------------\n Operating Time')
+print(' Ideal Cycle Time × Total Count\n Performance = -------------------------------\n Operating Time')
 print(' \n ')
 print(' Good Count\nQuality = -----------\n Total Count')
 print(' \n ')
@@ -43,13 +33,14 @@ print('OEE = Availability X Performance X Quality')
 print()
 print('So for this project our product is:- Gear hobbing machine')
 print("Lets compare its OEE for 4 months")
+
 #Variables used
 info=['Availability','Performance','Quality','Total OEE']
 col=['Red','Blue','Orange','Green']
-12
+
 #Importing the files
 # April
-A1=pd.read_csv('D:\\IP PROJECT(Shreya and Sanya)\\April month.csv')
+A1=pd.read_csv('D:\\IP\\IP Project\\April month.csv')
 Apr=pd.DataFrame(A1)
 #print(Apr)
 a=Apr.iloc[30:,7:].T
@@ -62,21 +53,23 @@ plt.title('Records of April')
 plt.show()
 print(Fore.RED)
 print('Here the maximum loss is in Performance')
+
 #May
-M1=pd.read_csv('D:\\IP PROJECT(Shreya and Sanya)\\May month.csv')
+M1=pd.read_csv('D:\\IP\\IP Project\\May month.csv')
 May=pd.DataFrame(M1)
 #print(May)
 m=May.iloc[31:,7:].T
 M=m[31].values
-13
+
 #June
-J1=pd.read_csv('D:\\IP PROJECT(Shreya and Sanya)\\June month.csv')
+J1=pd.read_csv('D:\\IP\\IP Project\\June month.csv')
 June=pd.DataFrame(J1)
 #print(June)
 Jun=June.iloc[30:,7:].T
 JUN=Jun[30].values
+
 #July
-Jl=pd.read_csv('D:\\IP PROJECT(Shreya and Sanya)\\July month.csv')
+Jl=pd.read_csv('D:\\IP\\IP Project\\July month.csv')
 July=pd.DataFrame(Jl)
 #print(July)
 Jul=July.iloc[31:,7:].T
@@ -89,7 +82,6 @@ x=input('Enter your option:')
 print('----'*20)
 if x=='Yes':
  plt.figure(figsize=(10,5))
-14
  plt.bar(info,M,color=col,width=0.5)
  plt.ylim(70,100)
  plt.ylabel('in Percentage')
@@ -113,16 +105,11 @@ if x=='Yes':
  plt.show()
  print(Fore.MAGENTA)
  print('We can see the diversity in losses for the above months')
-15
- print('By looking at the graph we can know about the reasons for the losses in 
-the following months, and that is the main idea of OEE')
-else:
- print()
-print("Okay, now let's have a look at the final comparison of OEE of all the 
-months")
+ print('By looking at the graph we can know about the reasons for the losses in the following months and that is the main idea of OEE')
+
+print("Okay, now let's have a look at the final comparison of OEE of all the months")
 print() 
-df1=pd.DataFrame([A,M,JUN,JL],index=['April','May','June','July'], 
-columns=info)
+df1=pd.DataFrame([A,M,JUN,JL],index=['April','May','June','July'], columns=info)
 x=df1['Total OEE']
 y=df1.index
 l1=x.values.tolist()
@@ -130,18 +117,15 @@ l2=y.values.tolist()
 l3=[85]*4
 plt.figure(figsize=(14,7))
 plt.ylim(80,90)
-plt.plot(l2,l1,linewidth=4,marker='o',markersize=8,markeredgecolor= 
-'Red',label='Actual OEE')
+plt.plot(l2,l1,linewidth=4,marker='o',markersize=8,markeredgecolor = 'Red',label='Actual OEE')
 plt.plot(l3,linewidth=5,linestyle='dashed',label='Ideal OEE')
 plt.title('Comparison of all months')
 plt.legend()
 plt.show()
 print(Fore.BLUE)
 print('Here we can see that the values of OEE are below the ideal value, i.e, 85%')
-16
-print('But it also shows us the improvement which is due to the rectifications of 
-the error which we can know by comparing the\ graphs and having a look at the 
-area which causes the maximum losses')
+
+print('But it also shows us the improvement which is due to the rectifications of the error which we can know by comparing the graphs and having a look at the area which causes the maximum losses')
 print(Fore.RED)
 print('Lets try calculating our OEE performance')
 print('----'*20)
@@ -154,9 +138,10 @@ Ava=OT/PPT
 Per=(ICT*TC)/OT
 Qua=GC/TC
 OEE=Ava*Per*Qua
-d1={'Availability %':Ava*100,'Performance %':Per*100,'Quality 
-%':Qua*100,'OEE %':OEE*100}
+d1={'Availability %':Ava*100,'Performance %':Per*100,'Quality %':Qua*100,'OEE %':OEE*100}
 Df=pd.DataFrame(d1,index=[1])
 print('----'*20)
 print(Df)
-Df.to_csv('D:\\IP PROJECT(Shreya and Sanya)\\New values.csv',index=False)
+Df.to_csv('D:\\IP\\IP Project\\New values.csv',index=False)
+
+#by Shreya Rai
